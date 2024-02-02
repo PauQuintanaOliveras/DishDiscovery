7551145e833b8b4c156d0b91b3233f334d25c2f5
tuvi <alberttubert@inspladelestany.cat>
2024-02-02 17:25:41 +0100

    RecuperarContra 1.1

--- app/src/main/java/cat/dam/dishdiscovery/MainActivity.kt
+++ app/src/main/java/cat/dam/dishdiscovery/MainActivity.kt
@@ -44,7 +44,7 @@ fun UserInterface() {
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
