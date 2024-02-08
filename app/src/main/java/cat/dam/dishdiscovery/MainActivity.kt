14b8a73288b344f220828862d3a597bbaa3db58d
tuvi <alberttubert@inspladelestany.cat>
2024-02-08 18:17:18 +0100

    ErrorCharacters

--- app/src/main/java/cat/dam/dishdiscovery/MainActivity.kt
+++ app/src/main/java/cat/dam/dishdiscovery/MainActivity.kt
@@ -6,17 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Snackbar
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,6 +30,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import cat.dam.dishdiscovery.ui.theme.DishDiscoveryTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +39,7 @@ class MainActivity : ComponentActivity() {
             DishDiscoveryTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
                     val navController = rememberNavController()
                     LoginScreen(navController)
@@ -47,6 +54,8 @@ fun LoginScreen(navController: NavController) {
     val username = remember { mutableStateOf(TextFieldValue()) }
     val password = remember { mutableStateOf(TextFieldValue()) }
     val logo = painterResource(id = R.drawable.logo)
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
     Column(
         modifier = Modifier
@@ -86,14 +95,36 @@ fun LoginScreen(navController: NavController) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = { }) {
+        Button(onClick = {
+            if (!isValidInput(username.value.text) || !isValidInput(password.value.text)) {
+                // Muestra un mensaje de error
+                scope.launch {
+                    snackbarHostState.showSnackbar("L'usuari només pot accedir si afegeix a-z, A-Z, 0-9 o _")
+                }
+            } else {
+                // Continúa amb l' inici de sessio
+            }
+        }) {
             Text("Accedir")
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(text = "Registrar-se", modifier = Modifier.clickable { navController.navigate("register") })
+
+        // Añade el SnackbarHost al final de la Column
+        SnackbarHost(hostState = snackbarHostState) { data ->
+            Snackbar(
+                snackbarData = data,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
     }
+}
+
+fun isValidInput(input: String): Boolean {
+    val pattern = Regex("^[a-zA-Z0-9_]*$")
+    return pattern.matches(input)
 }
 
 @Preview(showBackground = true)
