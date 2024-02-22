320f19f02600b554d3930c792a2bbd8f77c34a50
Moha <mohamedkamara@inspladelestany.cat>
2024-02-22 19:13:58 +0100

    DropMenu

--- app/src/main/java/cat/dam/dishdiscovery/CrearRecepte.kt
+++ app/src/main/java/cat/dam/dishdiscovery/CrearRecepte.kt
@@ -1,19 +1,29 @@
 package cat.dam.dishdiscovery
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -81,6 +91,9 @@ class CrearRecepte {
                 fontSize = 10.sp,
                 onTextLayout = {}
             )
+            Spacer(modifier =Modifier.height(11.dp))
+            ShowExposedDropdownMenu()
+
             Spacer(modifier = Modifier.height(11.dp))
             TextField( // Add this TextField
                 value = productState.value,
@@ -142,9 +155,9 @@ class CrearRecepte {
             }
             Button(
                 onClick = { /* Handle button click */ },
-            modifier=Modifier
+            modifier= Modifier
                 .align(Alignment.CenterHorizontally)
-                .size(200.dp,50.dp)
+                .size(200.dp, 50.dp)
 
             ) {
                 Text( text = "Fet",
@@ -154,4 +167,47 @@ class CrearRecepte {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ShowExposedDropdownMenu() {
+        val context = LocalContext.current
+        val numbers = Array(12) { i -> (i + 1).toString() } // Array of numbers from 1 to 12
+        var expanded by remember { mutableStateOf(false) }
+        var selectedItemIndex by remember { mutableStateOf(0) }
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+            modifier = Modifier.padding(16.dp),
+        ) {
+            TextField(
+                value = numbers[selectedItemIndex],
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier.menuAnchor()
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                numbers.forEachIndexed { index, item ->
+                    DropdownMenuItem(
+                        text = {
+                            Text(
+
+                                text = item,
+                                fontWeight = if (index == selectedItemIndex) FontWeight.Bold else null,
+                                onTextLayout = {}
+                            )
+                        },
+                        onClick = {
+                            selectedItemIndex = index
+                            expanded = false
+                            Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                }
+            }
+        }
+    }
 }
