1f525013c21c380bcc86973e921a46dbe7dccc6b
Moha <mohamedkamara@inspladelestany.cat>
2024-02-23 18:00:17 +0100

    CrearRecepte

--- app/src/main/java/cat/dam/dishdiscovery/CrearRecepte.kt
+++ app/src/main/java/cat/dam/dishdiscovery/CrearRecepte.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -42,7 +41,7 @@ class CrearRecepte {
         val productState = remember { mutableStateOf("") }
         val checkboxState1 = remember { mutableStateOf(false) } // Add this line
         val checkboxState2 = remember { mutableStateOf(false) }
-        Column(
+       Column(
             modifier = Modifier.fillMaxWidth()
 
         ) {
