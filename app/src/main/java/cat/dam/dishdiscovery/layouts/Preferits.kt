b39428d8d3b3e0247c31f7a8ff1e7c7205e85b6c
Moha <mohamedkamara@inspladelestany.cat>
2024-03-06 18:56:46 +0100

    icona de mes

--- app/src/main/java/cat/dam/dishdiscovery/layouts/Preferits.kt
+++ app/src/main/java/cat/dam/dishdiscovery/layouts/Preferits.kt
@@ -20,7 +20,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -102,14 +104,25 @@ fun Preferits()
                             R.drawable.pasta,
 
                         )
+
                     }
 
 
                 }
             }
         },
+        floatingActionButton = {
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Add, contentDescription = "Add")
+                }
+},
 
         bottomBar = {
+
+
+
+            
+
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier.fillMaxWidth()
