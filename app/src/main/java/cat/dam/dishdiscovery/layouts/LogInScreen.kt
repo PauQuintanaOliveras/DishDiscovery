fe9e9947fa8add9658c614d472f06b4bc7c5dadf
Pau Quintana <pauquintana@inspladelestany.cat>
2024-03-07 17:29:46 +0100

    Android studio decidning to beack everything without reason fixed

--- app/src/main/java/cat/dam/dishdiscovery/layouts/LogInScreen.kt
+++ app/src/main/java/cat/dam/dishdiscovery/layouts/LogInScreen.kt
@@ -37,121 +37,119 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.GoogleAuthProvider
 
+
 @Composable
 fun LogInScreen(navController: NavController) {
-    @Composable
-    fun LoginScreen(navController: NavController) {
-        val username = remember { mutableStateOf(TextFieldValue()) }
-        val password = remember { mutableStateOf(TextFieldValue()) }
-        val logo = painterResource(id = R.drawable.logo)
-        val snackbarHostState = remember { SnackbarHostState() }
-        val loginViewModel: LoginScreenViewModel = viewModel()
-        val context = LocalContext.current
-        val launcher = rememberLauncherForActivityResult(
-            contract = ActivityResultContracts
-                .StartActivityForResult()
+    val username = remember { mutableStateOf(TextFieldValue()) }
+    val password = remember { mutableStateOf(TextFieldValue()) }
+    val logo = painterResource(id = R.drawable.logo)
+    val snackbarHostState = remember { SnackbarHostState() }
+    val loginViewModel: LoginScreenViewModel = viewModel()
+    val context = LocalContext.current
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts
+            .StartActivityForResult()
 
-        ) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
-            try {
-                val account = task.getResult(ApiException::class.java)
-                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-                loginViewModel.logInWithGoogleCredentials(credential) {
-                    navController.navigate("main_page") {
-                        // Pop up to the login screen, removing it from the back stack
-                        popUpTo("login_screen") {
-                            inclusive = true
-                        }
+    ) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+        try {
+            val account = task.getResult(ApiException::class.java)
+            val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+            loginViewModel.logInWithGoogleCredentials(credential) {
+                navController.navigate("main_page") {
+                    // Pop up to the login screen, removing it from the back stack
+                    popUpTo("login_screen") {
+                        inclusive = true
                     }
-
                 }
-            } catch (e: ApiException) {
-                Log.w("GoogleSignIn", "signInResult:failed code=" + e.statusCode)
+
             }
+        } catch (e: ApiException) {
+            Log.w("GoogleSignIn", "signInResult:failed code=" + e.statusCode)
         }
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = logo,
-                contentDescription = "Logo",
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            TextField(
-                value = username.value,
-                onValueChange = { username.value = it },
-                label = { Text("Correu Electronic") },
-                modifier = Modifier.fillMaxWidth(),
-                singleLine = true
-            )
-
-            TextField(
-                value = password.value,
-                onValueChange = { password.value = it },
-                label = { Text("Contrasenya") },
-                visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier.fillMaxWidth(),
-                singleLine = true
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Button(onClick = {
-                loginViewModel.logInWithEmailAndPassword(username.value.text, password.value.text) {
-                    navController.navigate("main_page")
-                }
-            }) {
-                Text("Accedir")
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Button(onClick = {
-                val options = GoogleSignInOptions.Builder(
-                    GoogleSignInOptions.DEFAULT_SIGN_IN
-                )
-                    .requestIdToken("819336651564-ataubgncel95p2i9or6j6ho51oluo0h6.apps.googleusercontent.com")
-                    .requestEmail()
-                    .build()
-                val signInClient = GoogleSignIn.getClient(context, options)
-                launcher.launch(signInClient.signInIntent)
-
-            }) {
-                Image(
-                    modifier = Modifier
-                        .padding(end = 8.dp)
-                        .size(24.dp),
-                    painter = painterResource(id = R.drawable.googlelogo), contentDescription = ""
-                )
-                Text("Accedir amb Google")
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Text(
-                text = "Registrar-se",
-                textDecoration = TextDecoration.Underline,
-                modifier = Modifier.clickable { navController.navigate("sign_in_screen") }
-
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Text(
-                text = "Has oblidat la contrasenya?",
-                textDecoration = TextDecoration.Underline,
-                modifier = Modifier.clickable { navController.navigate("recover_password_screen") }
-            )
-        }
-
-        SnackbarHost(hostState = snackbarHostState)
     }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = logo,
+            contentDescription = "Logo",
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = username.value,
+            onValueChange = { username.value = it },
+            label = { Text("Correu Electronic") },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+
+        TextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = { Text("Contrasenya") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            loginViewModel.logInWithEmailAndPassword(username.value.text, password.value.text) {
+                navController.navigate("main_page")
+            }
+        }) {
+            Text("Accedir")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            val options = GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN
+            )
+                .requestIdToken("819336651564-ataubgncel95p2i9or6j6ho51oluo0h6.apps.googleusercontent.com")
+                .requestEmail()
+                .build()
+            val signInClient = GoogleSignIn.getClient(context, options)
+            launcher.launch(signInClient.signInIntent)
+
+        }) {
+            Image(
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(24.dp),
+                painter = painterResource(id = R.drawable.googlelogo), contentDescription = ""
+            )
+            Text("Accedir amb Google")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Registrar-se",
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier.clickable { navController.navigate("sign_in_screen") }
+
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Has oblidat la contrasenya?",
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier.clickable { navController.navigate("recover_password_screen") }
+        )
+    }
+
+    SnackbarHost(hostState = snackbarHostState)
 }
