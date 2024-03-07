3deb3f4b670371c2ecdec733c015de2bfd3d7094
tuvi <alberttubert@inspladelestany.cat>
2024-03-07 15:54:32 +0100

    ErrorGradleSolucionat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore") //firebase
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
     implementation("io.coil-kt:coil-compose:1.4.0")
+    implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.compose.material3:material3-android:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
