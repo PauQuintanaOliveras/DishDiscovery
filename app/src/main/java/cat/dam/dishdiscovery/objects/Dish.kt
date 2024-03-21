aa345919f03e015463a81ab3118cef35b9515410
DuckSpa <ivanduque@inspladelestany.cat>
2024-03-21 16:03:30 +0100

    uncommented IngridientsQty

--- app/src/main/java/cat/dam/dishdiscovery/objects/Dish.kt
+++ app/src/main/java/cat/dam/dishdiscovery/objects/Dish.kt
@@ -7,14 +7,14 @@ data class Dish(
     val dishElaboration: MutableState<String>,
     val dishServings: Int,
     //val dishUrl: String,
-    //val ingridientsQty: Map<Ingridient, Mesurement>
+    val ingridientsQty: Map<Ingridient, Mesurement>
 ) {
     fun dishToMap(): Map<String, Any?> {
         return mapOf(
             "DishElaboration" to dishElaboration,
             "DishServings" to dishServings,
             //"DishUrl" to dishUrl,
-            //"IngridientsQty" to ingridientsQty
+            "IngridientsQty" to ingridientsQty
         )
     }
 }
